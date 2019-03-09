61c8521e6c6736f9c40e8274ab6832a3ec95b782
Unknown <vendera.hadi@gmail.com>
2019-03-10 00:15:22 +0700

    home page update

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -6,39 +6,39 @@ Home
 
 @section('content')
   <div id="app">
-    <section id="slider" class="container">
-      <div class="row d-flex justify-content-center">
-        <div class="col-sm-11">
-          <my-carousel :images='{!! $sliders !!}'/>
+    <section id="slider" class="container-fluid home-slider-bg">
+      <div class="container">
+        <div class="row d-flex justify-content-center">
+          <div class="col-sm-11">
+            <my-carousel :images='{!! $sliders !!}'/>
+          </div>
         </div>
       </div>
     </section>
 
-    <section id="product" class="container">
-      <div class="row d-flex justify-content-center">
-        <div class="col-sm-6">
-          <img src="{{asset('images/ediva_pure.jpg')}}" class="img-fluid" alt="">
+    <section id="product" class="container home-product-container">
+      <div class="row d-flex justify-content-between">
+        <div class="col-sm-6" data-aos="fade-right">
+          <img src="{{asset('images/ediva_pure.png')}}" class="img-fluid" alt="">
         </div>
-        <div class="col-sm-1"></div>
-        <div class="col-sm-4">
-          <h1>Ediva Pure</h1>
-          <p>Suplemen alami untuk menyeimbangkan masalah hormon dalam tubuh dan juga membantu meringankan gejala pre-menstruasi</p>
+        <div class="col-sm-4" data-aos="fade-left" data-aos-delay="1000">
+          <h1 class="text-gray text-custom">Ediva Pure</h1>
+          <p class="text-gray">Suplemen alami untuk menyeimbangkan masalah hormon dalam tubuh dan juga membantu meringankan gejala pre-menstruasi</p>
         </div>
       </div>
       <div class="row d-flex justify-content-center mt-5">
-        <div class="col-sm-4">
-          <h1>Ediva Young</h1>
-          <p>Suplemen alami yang dapat memelihara juga membantu mengurangi berbagai permasalahan yang timbul pada kulit</p>
+        <div class="col-sm-4" data-aos="fade-right" data-aos-delay="1000">
+          <h1 class="text-gray text-custom">Ediva Young</h1>
+          <p class="text-gray">Suplemen alami yang dapat memelihara juga membantu mengurangi berbagai permasalahan yang timbul pada kulit</p>
         </div>
-        <div class="col-sm-1"></div>
-        <div class="col-sm-6">
+        <div class="col-sm-6" data-aos="fade-left">
           <img src="{{asset('images/ediva_young.png')}}" class="img-fluid" alt="">
         </div>
       </div>
     </section>
 
     <section id="animate-desc" class="container-fluid py-5 my-5">
-      <div class="row d-flex justify-content-center">
+      <div class="row d-flex justify-content-center" data-aos="fade-up">
         <div class="col-sm-11">
           <h1 class="text-brown text-center">kebaikan alami untuk para wanita indonesia</h1>
         </div>
@@ -46,12 +46,110 @@ Home
           <p class="text-brown text-center">Bahan alami yang terkandung dalam Ediva memiliki berbagai manfaat yang mampu menjaga dan merawat tubuh dari dalam</p>
         </div>
       </div>
+
+      <div class="row d-flex justify-content-center mt-5">
+        <div class="col-sm-6">
+          <h1 class="text-red text-custom text-right px-5">Gac Fruit</h1>
+        </div>
+        <div class="col-sm-6">
+          <h1 class="text-orange text-custom px-5">Royal Jelly</h1>
+        </div>
+      </div>
+
+      <div class="row d-flex justify-content-center" style="margin-top: 300px">
+        <div class="col-sm-8">
+          <h1 class="text-gray text-custom text-center px-5">Evening Primrose Oil</h1>
+          <center><img src="{{asset('images/primrose.png')}}" width="350" class="img-fluid" alt="" data-aos="fade-down"></center>
+          <br>
+          <center><img src="{{asset('images/vit-e.png')}}" width="150" class="img-fluid" alt="" data-aos="fade-down"></center>
+        </div>
+      </div>
+
+      <div class="home-jelly-right" data-aos="fade-down"></div>
+      <div class="home-jelly-desc-1">
+        <div class="row" data-aos="fade-down-left">
+          <div class="col-sm-12">
+            <img src="{{asset('images/arrow-left.png')}}" width="110" class="img-fluid" alt="">
+          </div>
+          <div class="col-sm-12">
+            <h5 class="text-brown font-weight-bold w-100 my-3">Asam Amino</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="home-jelly-desc-2">
+        <div class="row" data-aos="fade-down-right">
+          <div class="col-sm-12">
+            <img src="{{asset('images/arrow-right.png')}}" width="110" class="img-fluid" alt="">
+          </div>
+          <div class="col-sm-12">
+            <h5 class="text-brown font-weight-bold w-100 my-3">Vitamin B Complex</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="home-jelly-desc-3">
+        <div class="row" data-aos="fade-down-right">
+          <div class="col-sm-12">
+            <img src="{{asset('images/arrow-right.png')}}" width="110" class="img-fluid" alt="">
+          </div>
+          <div class="col-sm-12">
+            <h5 class="text-brown font-weight-bold w-100 my-3">Enzim</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="home-fruit-left" data-aos="fade-down"></div>
+      <div class="home-fruit-desc-1">
+        <div class="row" data-aos="fade-down-left">
+          <div class="col-sm-12">
+            <img src="{{asset('images/arrow-left.png')}}" width="110" class="img-fluid" alt="">
+          </div>
+          <div class="col-sm-12">
+            <h5 class="text-brown font-weight-bold w-100 my-3">Mengandung Likopen</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="home-fruit-desc-2">
+        <div class="row" data-aos="fade-down-right">
+          <div class="col-sm-12">
+            <img src="{{asset('images/arrow-right.png')}}" width="110" class="img-fluid" alt="">
+          </div>
+          <div class="col-sm-12">
+            <h5 class="text-brown font-weight-bold w-100 my-3">Mengandung ekstrak karotenoid</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="home-primrose-desc-1">
+        <div class="row" data-aos="fade-down-left">
+          <div class="col-sm-12">
+            <img src="{{asset('images/arrow-left.png')}}" width="110" class="img-fluid" alt="">
+          </div>
+          <div class="col-sm-12">
+            <h5 class="text-brown font-weight-bold w-100 my-3">Omega 6 Fatty Acid</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="home-primrose-desc-2">
+        <div class="row" data-aos="fade-down-right">
+          <div class="col-sm-12">
+            <img src="{{asset('images/arrow-right.png')}}" width="110" class="img-fluid" alt="">
+          </div>
+          <div class="col-sm-12">
+            <h5 class="text-brown font-weight-bold w-100 my-3">Asam Gamma-linolenic</h5>
+          </div>
+        </div>
+      </div>
+
     </section>
 
     <section id="why-ediva" class="container-fluid py-5 my-5">
       <div class="row d-flex justify-content-center">
         <div class="col-sm-11">
-          <h1 class="text-brown text-center">Kenapa Ediva ?</h1>
+          <h1 class="text-brown text-center text-gray font-weight-bold">Kenapa Ediva ?</h1>
         </div>
       </div>
       <div class="container">
@@ -59,16 +157,16 @@ Home
         <div class="col-sm-6">
           <div class="row why-1 d-flex flex-row-reverse">
             <div class="col-sm-6">
-              <h3>Ediva Pure</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <h1 class="text-custom text-red font-weight-bold">Ediva Pure</h3>
+              <p>Kombinasi Vit E, Evening Primrose Oil, dan buah Gac dapat menjadi anti-inflamasi yang mengurangi nyeri pre menstruasi dan meningkatkan level antioksidan yang menurun saat haid. Sehingga membebaskan kamu dari rasa khawatir saat datang bulan.</p>
             </div>
           </div>
         </div>
         <div class="col-sm-6 mt-5">
           <div class="row why-2 d-flex flex-row-reverse">
             <div class="col-sm-6">
-              <h3>Ediva Young</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <h1 class="text-custom text-primary font-weight-bold">Ediva Young</h3>
+              <p>Kandungan alami dalam setiap kapsul Ediva Young memberikan manfaat untuk memberikan kolagen pada lapisan dalam kulit (dermis), sehingga proses regenerasi sel kulit akan tetap terjaga. Membuat kulit menjadi jauh lebih elastis, sehat dan tampak cerah.</p>
             </div>
           </div>
         </div>
